058d627d0f6d71f1ba58bded009aaf09c49a1547
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2024-04-16 15:04:52 +1000

    add. unit tests

--- test/testServer.js
+++ test/testServer.js
@@ -29,11 +29,6 @@ describe("test POST api", function () {
 
 describe("test DELETE api", function () {
   var url = "http://localhost:3000/api/cats/661e000b2fa33b2f5a4406ef";
-  const body = {
-    title: "Cat x",
-    path: "https://us.123rf.com/450wm/elenasoldatova/elenasoldatova1604/elenasoldatova160400010/57162639-three-kittens-animal-outside-summer-nature-pet-little.jpg",
-    desciption: "Three Cats Stock Photos and Images - 123RF",
-  };
 
   it("delete a cat", function (done) {
     request.delete({ url }, function (error, response, body) {
